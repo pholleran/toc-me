5caa8a0b08812c8a4ad08fa75fc7786460850e2b
Philip Holleran <pholleran@github.com>
2019-02-20 22:34:02 -0500

    add test for unformatted markdown files

--- test/index.test.js
+++ test/index.test.js
@@ -7,12 +7,15 @@ const _ = require('lodash')
 // Requiring our fixtures
 const tocFormatterAdded = require('./fixtures/push.toc_formatter_added')
 const tocsSectionAdded = require('./fixtures/push.toc_section_added')
+const noToc = require('./fixtures/push.no_toc')
 
 const commits = require('./fixtures/commits.get_df298')
 const compareCommits = require('./fixtures/compare.86f502_105088')
+const compareCommitsNoToc = require('./fixtures/compare.5af90a_508e83')
 
 const readmeContents = require('./fixtures/contents.get_README.md')
 const readmeSectionAddedContents = require('./fixtures/contents.get_README.section_added.md')
+const noTocContents = require('./fixtures/contents.get_no-toc.md')
 const configContents = require('./fixtures/contents.get_toc.yml')
 
 nock.disableNetConnect()
@@ -84,5 +87,24 @@ describe('Test toc-me', async () => {
       .reply(200)
 
     await probot.receive({name: 'push', payload: tocsSectionAdded}, 20000)
+  })
+
+  test('does nothing when markdown file is not properly formatted for toc', async () => {
+    // mock an installation token
+    nock('https://api.github.com')
+      .post('/app/installations/240195/access_tokens')
+      .reply(200, { token: 'test' })
+
+        // mock the compare endpoint
+    nock('https://api.github.com')
+      .get('/repos/pholleran/test-toc-me/compare/5af90a02b23e47df467d0a3f2b2dc2bd0433265e...508e833d9dc61783b7ab1f7675f433fa04ca6e33')
+      .reply(200, compareCommitsNoToc)
+  
+    // mock the request for the markdown file
+    nock('https://api.github.com')
+      .get('/repos/pholleran/test-toc-me/contents/no-toc.md?ref=no-update')
+      .reply(200, noTocContents)
+
+    await probot.receive({name: 'push', payload: noToc}, 20000)
   })
 })
